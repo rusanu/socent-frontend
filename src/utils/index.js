e5f43ff60124258d1a9e45368464b68028bed557
Claudiu Ceia <claudiu.ceia+1@gmail.com>
2016-11-15 00:22:31 +0000

    swap api uri

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,9 +1,17 @@
 import axios from 'axios';
+
+let _apiURI = null;
+if (process.env.NODE_ENV === 'development') {
+  _apiURI = location.protocol + '//' + location.hostname + ':3001/';
+} else {
+  _apiURI = 'https://socent.cezarneaga.eu/api/v1/';
+}
+
 function getEnterprises() {
-  return axios.get('https://socent.cezarneaga.eu/api/v1/enterprises?status=neq|10')
+  return axios.get(_apiURI + 'enterprises?status=neq|10')
 }
 function getEnterprise(id) {
-  return axios.get(`https://socent.cezarneaga.eu/api/v1/enterprises/${id}`)
+  return axios.get(`${_apiURI}enterprises/${id}`)
 }
 function getEnterprisesPublic() {
   return axios.get('/public')
